008fb11a4c3d90bcd4b7c6df8a35fe48be81608d
po4tion <po4tion0429@gmail.com>
2024-05-18 23:40:41 +0900

    chore: catchErrorToLogger에 프로세스 주석 추가하기

--- transform/catchErrorToLogger.ts
+++ transform/catchErrorToLogger.ts
@@ -13,14 +13,17 @@ const IDENTIFIER = {
 export default function transformer(file: FileInfo, { jscodeshift: j }: API) {
   const source = j(file.source);
 
+  // 1. catch문을 사용하는지 확인한다
   source.find(j.CatchClause).forEach((path) => {
     const param = path.node.param;
 
+    // 2. catch문이 파라미터를 사용하는지 확인한다
     if (param) {
       // param이 Identifier 타입인지 확인
       if (param.type === "Identifier") {
         const { name: paramName } = param;
 
+        // 3. catch문이 파라미터를 사용하면 catch문 내에서 console.error를 사용하는지 확인한다
         j(path)
           .find(j.CallExpression, {
             callee: {
@@ -31,6 +34,7 @@ export default function transformer(file: FileInfo, { jscodeshift: j }: API) {
           .forEach((consolePath) => {
             const args = consolePath.node.arguments;
 
+            // 4. console.error의 첫번째 인자가 객체 표현식인지 확인한다
             if (args.length === 1 && args[0].type === "ObjectExpression") {
               const properties = args[0].properties;
               const hasErrorProperty = properties.some((property) => {
@@ -43,9 +47,10 @@ export default function transformer(file: FileInfo, { jscodeshift: j }: API) {
                 return false;
               });
 
+              // 5. console.error의 첫번째 인자에 error가 이미 입력되어 있는지 확인한다.
               if (!hasErrorProperty) {
                 if (paramName === IDENTIFIER.error) {
-                  // 매개변수 이름이 'error'일 때 객체 속성 축약 문법을 사용한다
+                  // 7. 매개변수 이름이 'error'일 때 객체 속성 축약 문법을 사용한다
                   properties.push(
                     j.property.from({
                       kind: "init",
@@ -55,6 +60,7 @@ export default function transformer(file: FileInfo, { jscodeshift: j }: API) {
                     })
                   );
                 } else {
+                  // 6. 입력되어 있지 않다면 error를 키값으로 하고 catch의 에러 파라미터 이름을 값으로 추가한다
                   properties.push(
                     j.property(
                       "init",
@@ -70,5 +76,6 @@ export default function transformer(file: FileInfo, { jscodeshift: j }: API) {
     }
   });
 
+  // 8. 소스코드를 수정한다
   return source.toSource();
 }
